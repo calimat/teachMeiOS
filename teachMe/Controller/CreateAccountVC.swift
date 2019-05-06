1a0db7f8f9e8b216b8ae5a5bf042c2718d0b6a83
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-05-05 19:52:22 -0500

    Created UITests to for login flow

--- teachMe/Controller/CreateAccountVC.swift
+++ teachMe/Controller/CreateAccountVC.swift
@@ -12,8 +12,10 @@ class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
---
+++ teachMeUITests/teachmeLoginFlowUITests.swift
@@ -0,0 +1,47 @@
+//
+//  teachmeLoginFlowUITests.swift
+//  teachMeUITests
+//
+//  Created by Ricardo Herrera Petit on 5/5/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+
+class teachmeLoginFlowUITests : XCTestCase {
+    
+    var application:XCUIApplication!
+    
+    override func setUp() {
+        continueAfterFailure = false
+        application = XCUIApplication()
+        application.launch()
+    }
+    
+    override func tearDown() {
+        application = nil
+    }
+    
+    func test_UserShouldSeeLoginVCController() {
+       XCTAssertTrue(application.textFields["email"].exists)
+    }
+    
+    func test_CreateAccountButton_ShouldShowCreateAccountVC() {
+       navigateToCreateAccountVC()
+        XCTAssertTrue(application.staticTexts["Would you like to create an acocunt for:"].exists)
+    }
+    
+    func testCreateAccountBackBtn_ShouldReturntoLoginVC() {
+        navigateToCreateAccountVC()
+        application.buttons["backBtnToLoginVC"].tap()
+        XCTAssertTrue(application.buttons["Don't Have an Account? Tap Here"].exists)
+    }
+    
+    func navigateToCreateAccountVC() {
+        let createAccountBtn = application.buttons["Don't Have an Account? Tap Here"]
+        createAccountBtn.tap()
+    }
+    
+}
