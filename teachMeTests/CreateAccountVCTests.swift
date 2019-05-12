ed88269e6282222c1c680816e3376799189784e5
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-05-12 12:25:37 -0500

    Started the creating the CreateAccountVC implementations

---
+++ teachMeTests/CreateAccountVCTests.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountVC.swift
+//  teachMeTests
+//
+//  Created by Ricardo Herrera Petit on 5/12/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
