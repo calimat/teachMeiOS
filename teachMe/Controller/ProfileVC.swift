1fe6ae9b00d2e1adbee79f9da600d99e94c331be
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-05-14 22:09:17 -0500

    Setting up integration tests to authenticate users, and also to delete them with firebase and firestore

---
+++ teachMe/Controller/ProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileVC.swift
+//  teachMe
+//
+//  Created by Ricardo Herrera Petit on 5/12/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ teachMeTests/IntegrationTests/Firebase/IntegrationTests.swift
@@ -0,0 +1,9 @@
+//
+//  IntegrationTests.swift
+//  teachMeTests
+//
+//  Created by Ricardo Herrera Petit on 5/13/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
