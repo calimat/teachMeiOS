293ec3cabff16c6a9465826ef06e37e0dbc44058
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-05-19 01:19:42 -0500

    Testing conditions in script

--- scripts/travis/testflight.sh
+++ scripts/travis/testflight.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#if [[ "$TRAVIS_PULL_REQUEST" != "true"  ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "true"  ]]; then
 echo "This is not a pull request. No deployment will be done."
-#exit 0
-#fi
-#if [[ "$TRAVIS_BRANCH" != "development" ]]; then
-#echo "Testing on a branch other than development. No deployment will be done."
-#exit 0
-#fi
+exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "development" ]]; then
+echo "Testing on a branch other than development. No deployment will be done."
+exit 0
+fi
+echo "Should run"
 #fastlane beta
